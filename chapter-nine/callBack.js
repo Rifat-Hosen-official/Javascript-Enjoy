d2a15549f15cbd4a29343b747f7650fbc593b46c
Rifat-Hosen-official <rifathosenofficial@gmail.com>
2023-07-30 13:11:41 +0600

    new code add

---
+++ chapter-nine/callBack.js
@@ -0,0 +1,21 @@
+function sample (a, b ,cd ){
+    var c = a + b 
+    var d = a - b
+    var result =cd (c, d)
+    return result 
+}
+
+function sum (a , b ){
+    return a + b 
+}
+var result = sample(5,8 , sum)
+console.log(result)
+
+var result2 = sample(5, 8 ,function(c,d ){
+    return c - d
+})
+console.log(result2)
+var result3 = sample(5, 8 ,function(c,d ){
+    return c * d
+})
+console.log(result3)
---
+++ chapter-nine/foreach.js
@@ -0,0 +1,8 @@
+var arr = [1, 2, 3, 4, 5]
+
+ sum = 0
+arr.forEach(function (value, index,arr){
+    // console.log(value)
+    sum += value
+})
+console.log(sum)
