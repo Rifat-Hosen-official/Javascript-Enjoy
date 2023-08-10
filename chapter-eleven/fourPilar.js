749e03ad8cd370d4dc72f0abae147e6db1005cc0
rifathosenofficial <rifathosenofficial@gmail.com>
2023-08-10 22:02:19 +0600

    oop programming code added

---
+++ chapter-eleven/fourPilar.js
@@ -0,0 +1,6 @@
+/**
+ * 1:Inheritance
+ * 2.Polymorphism
+ * 3.abstraction
+ * 4.Encapsulation
+*/
---
+++ chapter-twelve/index.js
@@ -0,0 +1,13 @@
+var rect = {
+    width: 200,
+    height: 300,
+
+    draw: function(){
+        console.log(' i am rectangle')
+        console.log(' my width is' + this.width)
+        console.log(' my height  is' + this.height)
+    }
+
+}
+
+rect.draw()
